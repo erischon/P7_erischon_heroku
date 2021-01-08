8d79cd29fe846e6ffdc8a588dddf7f48e1d3d40a
Eri Schon <erischon@gmail.com>
2021-01-08 14:04:45 +0100

    Update test_wrapper_wiki.py

--- tests/test_wrapper_wiki.py
+++ tests/test_wrapper_wiki.py
@@ -1,10 +1,7 @@
 from grandpy.wrapper_wiki import WrapperWiki
+from tests.wiki_result import wiki_pageid_result, wiki_text_result, location, coord, pageid
 
 wwiki = WrapperWiki()
-
-location = {'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}}
-coord = "48.76569917989272|2.392394129892722"
-pageid = "1509079"
 
 def test_location_to_coord():
     """ I test the formatting of location coordonates. """
@@ -13,12 +10,24 @@ def test_location_to_coord():
     assert result_a == "<class 'str'>"
     assert result_b == "48.76569917989272|2.392394129892722"
 
-def test_coord_to_pageid():
+def test_coord_to_pageid(monkeypatch):
     """ I test if there is a pageid. """
-    result_a = str(type(wwiki.coord_to_pageid(coord)))
-    assert result_a == "<class 'int'>"
+    class MockRequestGetPageID:
+        def __init__(self, url, params=None, headers=None):
+            pass 
+        def json(self):
+            return wiki_pageid_result
 
-def test_wiki_text():
-    """ """
-    result_a = str(type(wwiki.wiki_text(pageid)))
-    assert result_a == "<class 'dict'>"
+    monkeypatch.setattr('requests.get', MockRequestGetPageID)
+    assert isinstance(wwiki.coord_to_pageid(coord), int)
+
+def test_wiki_text(monkeypatch):
+    """ I test if there is a text. """
+    class MockRequestGetText:
+        def __init__(self, url, params=None, headers=None):
+            pass 
+        def json(self):
+            return wiki_text_result
+
+    monkeypatch.setattr('requests.get', MockRequestGetText)
+    assert isinstance(wwiki.wiki_text(pageid), dict)

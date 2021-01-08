d7a551cb18a1b75d8a50633e2004930e2c6ca509
Eri Schon <erischon@gmail.com>
2021-01-08 14:04:48 +0100

    Create wiki_result.py

---
+++ tests/wiki_result.py
@@ -0,0 +1,11 @@
+location = {'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}}
+
+coord = "48.76569917989272|2.392394129892722"
+
+pageid = "5548980"
+
+wiki_pageid_result = {'batchcomplete': '', 'query': {'geosearch': [{'pageid': 1509079, 'ns': 0, 'title': 'Thiais', 'lat': 48.765, 'lon': 2.3923, 'dist': 78.1, 'primary': ''}]}}
+{'batchcomplete': '', 'query': {'geosearch': [{'pageid': 1509079, 'ns': 0, 'title': 'Thiais', 'lat': 48.765, 'lon': 2.3923, 'dist': 78.1, 'primary': ''}]}}
+
+wiki_text_result = {'batchcomplete': '', 'query': {'pages': {'5548980': {'pageid': 5548980, 'ns': 0, 'title': 'Place de Ménilmontant', 'extract': 'La place de Ménilmontant est une voie située dans le quartier de Belleville du 20e arrondissement de Paris.'}}}}
+
